67b6310e7bab76796d2ce1e2438fd8d00faabb45
dekuyou <devnull@localhost>
2010-04-16 23:28:28 +0900

    chrome extensions

---
+++ jp_ddo_dekuyou_autoscroll.js
@@ -0,0 +1,18 @@
+
+
+
+window.addEventListener('click', function(e){
+        chrome.extension.sendRequest( {dekuyou:'1'}, function(res){
+
+		});
+
+}, false);
+
+
+
+window.addEventListener('keydown', function(e){
+        chrome.extension.sendRequest( {dekuyou:'1'}, function(res){
+
+		});
+
+}, false);
